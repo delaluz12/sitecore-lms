c5569ad6c0ddc70209bc2f0a831fc588588c5b39
Maria Rico <maria.rico@sitecore.com>
2024-06-27 16:43:00 -0500

    LMS-26103 update promo banner/modal
    updated modal/banner message; changed location of modal from 'home' to 'products'; promo banner/modal visibility now includes partners

--- src/UI/Buyer/src/app/components/layout/home/home.component.ts
+++ src/UI/Buyer/src/app/components/layout/home/home.component.ts
@@ -2,9 +2,6 @@ import { faBullhorn } from '@fortawesome/free-solid-svg-icons'
 import { Component, OnInit } from '@angular/core'
 import { ShopperContextService } from 'src/app/services/shopper-context/shopper-context.service'
 import { HSMeProduct } from '@ordercloud/headstart-sdk'
-import { PromoModalComponent } from '../../promo-modal/promo-modal.component'
-import { MatDialog } from '@angular/material/dialog'
-import { AppConfig } from 'src/app/models/environment.types'
 
 @Component({
   templateUrl: './home.component.html',
@@ -14,13 +11,8 @@ export class OCMHomePage implements OnInit {
   featuredProducts: HSMeProduct[]
   faBullhorn = faBullhorn
   URL = '../../../assets/jumbotron.svg'
-  closeResult = ''
 
-  constructor(
-    private context: ShopperContextService,
-    private dialog: MatDialog,
-    private appConfig: AppConfig
-  ) {}
+  constructor(private context: ShopperContextService) {}
 
   async ngOnInit(): Promise<void> {
     const user = this.context.currentUser.get()
@@ -32,36 +24,6 @@ export class OCMHomePage implements OnInit {
       })
       this.featuredProducts = products.Items
     }
-
-    const shownModal = sessionStorage.getItem('modalShown')
-    const showCustModal = this.checkUserGroups()
-    if (!shownModal && showCustModal) {
-      this.openAlertDialog()
-      sessionStorage.setItem('modalShown', 'true')
-    }
-  }
-
-  checkUserGroups(): boolean {
-    const env =
-      this.appConfig.sellerName == 'Sitecore LMS TEST' ? 'test' : 'prod'
-    // only show for userGroups = 0001-0008 or 0001-0005 (PROD)
-    const user = this.context.currentUser.get()
-    if (env === 'prod') {
-      return user?.UserGroups.some(
-        (item) => item.ID === '0001-0008' || item.ID === '0001-0005'
-      )
-    } else {
-      return user?.UserGroups.some(
-        (item) => item.ID === '0002-0008' || item.ID === '0002-0005'
-      )
-    }
-  }
-
-  openAlertDialog(): void {
-    const dialogRef = this.dialog.open(PromoModalComponent, {
-      height: 'auto',
-      width: 'auto',
-    })
   }
 
   toSupplier(supplier: string): void {
